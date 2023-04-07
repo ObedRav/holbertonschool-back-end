3326d3432d9681cb3b7027919474e6240e492205
ObedRav <obedrayo2356@gmail.com>
2023-04-07 13:52:06 +0000

    Fixing bugs

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -13,6 +13,7 @@ API_URL = 'https://jsonplaceholder.typicode.com'
 user_data = requests.get(f'{API_URL}/users/{argv[1]}/')
 user_tasks = requests.get(f'{API_URL}/users/{argv[1]}/todos')
 
+
 def byte_to_dict(byte):
     """
     FUNCTION
@@ -20,17 +21,19 @@ def byte_to_dict(byte):
     byte_str = byte.decode('utf-8')
     return json.loads(byte_str)
 
+
 if user_data.status_code == 200 and user_tasks.status_code == 200:
     user_name = byte_to_dict(user_data.content).get('name')
     user_tasks_dict = json.loads(user_tasks.content)
     tasks_done = [task for task in user_tasks_dict if task.get('completed')]
 
-    first_line = f"Employee {user_name} is done with tasks({len(tasks_done)}/{len(user_tasks_dict)}):"
+    first_line = \
+        f"Employee {user_name} is done with tasks\
+            ({len(tasks_done)}/{len(user_tasks_dict)}):"
 
-    print(first_line)
+    print(first_line.replace('            ', ' '))
     for task in tasks_done:
         print('\t {}'.format(task.get('title')))
-    
+
 else:
     print('Error:', user_data.status_code)
-
