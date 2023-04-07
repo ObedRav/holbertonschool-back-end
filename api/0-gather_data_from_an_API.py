a29085675cf8234b7fe997aa703d0ec2462bb74d
ObedRav <obedrayo2356@gmail.com>
2023-04-07 13:49:30 +0000

    Adding api manipulation

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module doc
+"""
+import requests
+from sys import argv
+import json
+from functools import reduce
+
+
+API_URL = 'https://jsonplaceholder.typicode.com'
+
+user_data = requests.get(f'{API_URL}/users/{argv[1]}/')
+user_tasks = requests.get(f'{API_URL}/users/{argv[1]}/todos')
+
+def byte_to_dict(byte):
+    """
+    FUNCTION
+    """
+    byte_str = byte.decode('utf-8')
+    return json.loads(byte_str)
+
+if user_data.status_code == 200 and user_tasks.status_code == 200:
+    user_name = byte_to_dict(user_data.content).get('name')
+    user_tasks_dict = json.loads(user_tasks.content)
+    tasks_done = [task for task in user_tasks_dict if task.get('completed')]
+
+    first_line = f"Employee {user_name} is done with tasks({len(tasks_done)}/{len(user_tasks_dict)}):"
+
+    print(first_line)
+    for task in tasks_done:
+        print('\t {}'.format(task.get('title')))
+    
+else:
+    print('Error:', user_data.status_code)
+
