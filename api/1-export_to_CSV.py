faa196a0393d02cf2664fd649522ca006daeaba2
ObedRav <obedrayo2356@gmail.com>
2023-04-13 17:31:39 +0000

    Fixing a bug

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -5,29 +5,30 @@ import requests
 from sys import argv
 
 
-API_URL = 'https://jsonplaceholder.typicode.com'
+if __name__ == "__main__":
+    API_URL = 'https://jsonplaceholder.typicode.com'
 
-user_id = argv[1]
-response = \
-    requests.get(
-        f'{API_URL}/users/{user_id}/todos',
-        params={'_expand': 'user'}
-    )
+    user_id = argv[1]
+    response = \
+        requests.get(
+            f'{API_URL}/users/{user_id}/todos',
+            params={'_expand': 'user'}
+        )
 
-if response.status_code == 200:
-    data = response.json()
-    username = data[0]['user']['username']
+    if response.status_code == 200:
+        data = response.json()
+        username = data[0]['user']['username']
 
-    with open(f"{user_id}.csv", "w", encoding='utf-8', newline="") as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        for task in data:
-            writer.writerow(
-                [
-                    f"{user_id}",
-                    f"{username}",
-                    f"{task['completed']}",
-                    f"{task['title']}"
-                ]
-            )
-else:
-    print(f"Error: {response.status_code}")
+        with open(f"{user_id}.csv", "w", encoding='utf-8', newline="") as file:
+            writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+            for task in data:
+                writer.writerow(
+                    [
+                        f"{user_id}",
+                        f"{username}",
+                        f"{task['completed']}",
+                        f"{task['title']}"
+                    ]
+                )
+    else:
+        print(f"Error: {response.status_code}")
